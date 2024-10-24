65a6e5633df7275ec4e234c54eca3c277c90c65a
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2024-10-24 23:17:35 +0300

    Add files via upload

---
+++ Frontend/src/Pages/RegisterPage.js
@@ -0,0 +1,17 @@
+import RegisterForm from "../Component/Register/RegisterForm"
+import WebsiteTemplate from "../Component/WebsiteTemplate.js/WebsiteTemplate"
+
+
+function RegisterPage(){
+    
+    return (
+        <div>
+
+            <RegisterForm/>
+            <WebsiteTemplate/>
+
+        </div>
+    )
+}
+
+export default RegisterPage
