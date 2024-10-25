e06b1f92826ddcf44194dac058caa96744519354
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2024-10-26 00:11:55 +0300

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import RegisterPage from '../src/Pages/RegisterPage'
-import LoginPage from './Pages/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PageNotFound from "../src/Pages/PageNotFound"
 
 import './App.css';
+import LoginPage from './Pages/LoginPage';
 
 function App() 
 {
   return (
     <div className="App">
-    // {<LoginPage/>}
-     <RegisterPage/>
+    <BrowserRouter>
+    <Routes>
+
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="*" element ={<PageNotFound/>} />
+    </Routes>
+    
+    </BrowserRouter>
     </div>
   );
 }
