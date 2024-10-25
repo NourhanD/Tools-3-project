fd6fc9fc30469d2a86087bcb8af64d01ecd7cc02
youssef ammar <128386855+ysfd1@users.noreply.github.com>
2024-10-25 12:30:44 +0300

    Add files via upload

---
+++ Frontend/src/Pages/LoginPage.js
@@ -0,0 +1,16 @@
+import LoginForm from "../Component/Login/LoginForm";
+import WebsiteTemplate from "../Component/WebsiteTemplate.js/WebsiteTemplate"
+
+
+function LoginPage () {
+    return (
+        <div>
+            <LoginForm>
+
+            </LoginForm>
+           < WebsiteTemplate/>
+
+        </div>
+    )
+}
+export default LoginPage 
