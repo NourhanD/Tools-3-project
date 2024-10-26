5c1ac947e8f957171869693a40aca271516704b2
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-10-26 19:40:33 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,16 @@
 const User = require('./Model/user');
 const express = require('express');
 const pool = require('./db');
+const cors = require('cors'); 
+const dotenv = require('dotenv');
 const app = express();
+
 const PORT = process.env.PORT || 5000; 
+
+dotenv.config();
+app.use(cors());
 app.use(express.json());
+
 app.post('/register', async (req, res) => {
   const { firstName, lastName, phoneNumber, email, password } = req.body;
   try {
