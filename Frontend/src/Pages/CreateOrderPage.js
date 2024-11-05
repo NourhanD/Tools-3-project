5f128f2005ac4320617285c291513115e4f845e7
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2024-11-05 18:51:38 +0200

    Add files via upload

---
+++ Frontend/src/Pages/CreateOrderPage.js
@@ -0,0 +1,15 @@
+import CreateOrder from "../Component/CreateOrder/CreateOrder"
+import WebsiteTemplate from "../Component/WebsiteTemplate/WebsiteTemplate"
+
+function CreateOrderPage()
+{
+    return (
+        <div>
+             <CreateOrder/>
+             <WebsiteTemplate/>
+        </div>
+       
+    )
+}
+
+export default CreateOrderPage
---
+++ Frontend/src/Pages/UserHomePage.js
@@ -0,0 +1,33 @@
+import WebsiteTemplate from '../Component/WebsiteTemplate/WebsiteTemplate';
+import createOrderIcon from '../Component/Icons/orders-icon-5.jpg';
+import previousOrders from '../Component/Icons/Screenshot-2022-11-24-132227.png';
+import { useNavigate } from 'react-router-dom';
+
+function UserHomePage() {
+    const navigate = useNavigate()
+    return (
+        <div>
+            <WebsiteTemplate />
+            <div style={{ position: "absolute", top: "60px", left: "30px" }}>
+                <h3 style={{ fontSize: "40px" ,fontWeight:"bold" }}>Hello username</h3>
+            </div>
+            
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", gap: "120px" }}>
+                <div>
+                <img src={createOrderIcon} className="card-img-top" alt="createOrderIcon" style={{ width: "100%", height: "250px", objectFit:'scale-down', margin: "0 auto" }} />
+                <button className="btn btn-outline-secondary btn-lg" onClick={() => navigate('/createOrderPage')} style={{ fontWeight: "bold" , marginTop:"25px" , fontSize:"25px"  }} >Start ordering</button>
+                </div>
+
+            
+                <div>
+                <img src={previousOrders} className="card-img-top" alt="previousOrders" style={{ width: "90%", height: "250px", objectFit:"contain", margin: "0 auto" }} />
+                <button className="btn btn-outline-secondary btn-lg" style={{ fontWeight: "bold" , marginTop:"20px" , fontSize:"25px" }} >My orders</button>
+                </div> 
+               
+                </div>
+         
+        </div>
+    );
+}
+
+export default UserHomePage;
