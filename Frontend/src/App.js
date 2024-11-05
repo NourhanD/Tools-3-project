a3da8962a12becd219b32b96ba8008f996d75895
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2024-11-05 18:52:19 +0200

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import RegisterPage from '../src/Pages/RegisterPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PageNotFound from "../src/Pages/PageNotFound"
-
+import UserHomePage from './Pages/UserHomePage'
+import CreateOrderPage from './Pages/CreateOrderPage'
 import './App.css';
 import LoginPage from './Pages/LoginPage';
 
@@ -16,6 +17,9 @@ function App()
     <Route path="/" element={<LoginPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/register" element={<RegisterPage />} />
+    <Route path="/home" element={<UserHomePage/>} />
+    <Route path="/createOrderPage" element={<CreateOrderPage/>} />
+    
     <Route path="*" element ={<PageNotFound/>} />
     </Routes>
     
