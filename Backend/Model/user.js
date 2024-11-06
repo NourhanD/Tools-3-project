791508f0f39fa6272b672779f59920cee3abe237
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-07 00:42:02 +0200

    Update user.js

--- Backend/Model/user.js
+++ Backend/Model/user.js
@@ -1,11 +1,11 @@
-
 const pool = require('../db'); 
 
 class User {
-  static async register(firstName, lastName, phoneNumber, email, password) {
+  static async register(firstName, lastName, phoneNumber, email, password, role) {
 
-    const query = 'INSERT INTO "User" ("firstName", "lastName", "phoneNumber", "email", "password") VALUES ($1, $2, $3, $4, $5) RETURNING *'; 
-    const values = [firstName, lastName, phoneNumber, email, password];
+    const query = 'INSERT INTO "User" ("firstName", "lastName", "phoneNumber", "email", "password", "role") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *'; 
+    const values = [firstName, lastName, phoneNumber, email, password, role];
+
     try {
       const result = await pool.query(query, values);
       return result.rows[0];
@@ -14,6 +14,7 @@ class User {
       throw err; 
     }
   }
+  
   static async findByEmail(email) {
     const query = 'SELECT * FROM "User" WHERE "email" = $1';
     const values = [email];
