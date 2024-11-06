0d38fca691d69337e0476c66d2e1d007832df2c5
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-07 00:41:18 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,47 +1,23 @@
-const User = require('./Model/user');
+const userController = require('./Controller/userController');
+const authenticate = require('./Middleware/authentication');
 const express = require('express');
-const pool = require('./db');
 const cors = require('cors'); 
+const pool = require('./db');
 const dotenv = require('dotenv');
 const app = express();
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', async (req, res) => {
-  const { firstName, lastName, phoneNumber, email, password } = req.body;
-  try {
-    const newUser = await User.register(firstName, lastName, phoneNumber, email, password);
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to create user' });
-  }
-});
+app.post('/register', userController.register);
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+app.post('/login',userController.login);
 
-  try {
-    
-    const user = await User.findByEmail(email);
+app.post('/order', authenticate, userController.createOrder)
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    if (password !== user.password) {
-      console.error('Invalid password');
-      return res.status(400).json({ error: 'Invalid password' });
-    }
-    res.status(200).json({ message: 'Logged in successful' });
-  } 
-  catch (err) {
-    res.status(500).json({ error: 'Login failed' });
-  }
-});
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
@@ -54,4 +30,5 @@ if (process.env.NODE_ENV !== 'test') {
     }
   });
 }
+
 module.exports = app;
