89e1af1bd98893ffac942e65670db628f78cd7f8
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-07 00:44:59 +0200

    Update userController.js

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,16 +1,20 @@
-const User = require('./model/User');
+const User = require('../Model/user');
+const Order = require('../Model/Order');
+const jwt = require('jsonwebtoken');
 
-// Create a new user
+// Create a user
 const register = async (req, res) => {
-  const { firstName, lastName, phoneNumber, email, password  } = req.body;
+  const { firstName, lastName, phoneNumber, email, password, role } = req.body;
+
   try {
-    const newUser = await User.createUser(firstName, lastName, phoneNumber, email, password );
+    const newUser = await User.register(firstName, lastName, phoneNumber, email, password, role);
     res.status(201).json(newUser);
   } catch (err) {
     console.error('Error creating user:', err);
     res.status(500).json({ error: 'Failed to create user' });
   }
 };
+
 // user login
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -22,13 +26,12 @@ const login = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
     
-    if (!isPasswordValid) {
+    if (password !== user.password) {
       return res.status(400).json({ error: 'Invalid password' });
     }
-    res.status(200).json({ message: 'Login successful', user, token });
+    const token = jwt.sign({ id : user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ message: 'Logged in successful', user, token });
   } 
   catch (err) 
   {
@@ -37,7 +40,27 @@ const login = async (req, res) => {
   }
 };
 
+// Create an order
+const createOrder = async (req, res) => {
+  try {
+    const userId = req.user.id; 
+    
+    const { pickup_loc, dropoff_loc,  package_details, delivery_time } = req.body;
+
+    if (!pickup_loc || !dropoff_loc || !package_details || !delivery_time) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const newOrder = await Order.createOrder(userId, {pickup_loc, dropoff_loc, package_details, delivery_time, status: 'Pending'});
+    res.status(201).json(newOrder);
+} catch (err) {
+    console.error('Error creating order:', err);
+    res.status(500).json({ error: 'Failed to create order' });
+}
+};
+
 module.exports = {
   register,
-  login
+  login,
+  createOrder
 };
