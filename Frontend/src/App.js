499ffe303e8276255aa43aeec4736ec232a6bece
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-08 00:31:16 +0200

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import RegisterPage from '../src/Pages/RegisterPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PageNotFound from "../src/Pages/PageNotFound"
 import UserHomePage from './Pages/UserHomePage'
 import CreateOrderPage from './Pages/CreateOrderPage'
