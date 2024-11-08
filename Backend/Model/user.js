6f5bd65341fa6f99d31268de57a4d57e8ff02b8c
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-08 22:02:05 +0200

    Update user.js

--- Backend/Model/user.js
+++ Backend/Model/user.js
@@ -2,9 +2,13 @@ const pool = require('../db');
 
 class User {
   static async register(firstName, lastName, phoneNumber, email, password, role) {
-
-    const query = 'INSERT INTO "User" ("firstName", "lastName", "phoneNumber", "email", "password", "role") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *'; 
-    const values = [firstName, lastName, phoneNumber, email, password, role];
+    let query;
+      if (role === 'courier') {
+        query = 'INSERT INTO "Courier" ("firstName", "lastName", "phoneNumber", "email", "password") VALUES ($1, $2, $3, $4, $5) RETURNING *';
+      } else {
+        query = 'INSERT INTO "User" ("firstName", "lastName", "phoneNumber", "email", "password", "role") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+      }
+      const values = role === 'courier' ? [firstName, lastName, phoneNumber, email, password] : [firstName, lastName, phoneNumber, email, password, role];
 
     try {
       const result = await pool.query(query, values);
@@ -16,12 +20,19 @@ class User {
   }
   
   static async findByEmail(email) {
-    const query = 'SELECT * FROM "User" WHERE "email" = $1';
-    const values = [email];
-
+    const queries = [
+      { query: 'SELECT * FROM "User" WHERE "email" = $1', role: 'user' },
+      { query: 'SELECT * FROM "Courier" WHERE "email" = $1', role: 'courier' }
+    ];
+    const values = [email];  
+  
     try {
-      const result = await pool.query(query, values);
-      return result.rows[0]; 
+      for (const { query, role } of queries) {
+        const result = await pool.query(query, values);
+        if (result.rows.length > 0) {
+          return { ...result.rows[0], role };
+        }
+      }
     } catch (err) {
       console.error('Error finding user by email:', err);
       throw err;
