ca638a8f52021bc537238a02e50f7c1da088f2a0
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-09 01:09:11 +0200

    Update user.js

--- Backend/Model/user.js
+++ Backend/Model/user.js
@@ -3,12 +3,12 @@ const pool = require('../db');
 class User {
   static async register(firstName, lastName, phoneNumber, email, password, role) {
     let query;
-      if (role === 'courier') {
+      if (role === 'Courier') {
         query = 'INSERT INTO "Courier" ("firstName", "lastName", "phoneNumber", "email", "password") VALUES ($1, $2, $3, $4, $5) RETURNING *';
       } else {
         query = 'INSERT INTO "User" ("firstName", "lastName", "phoneNumber", "email", "password", "role") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
       }
-      const values = role === 'courier' ? [firstName, lastName, phoneNumber, email, password] : [firstName, lastName, phoneNumber, email, password, role];
+      const values = role === 'Courier' ? [firstName, lastName, phoneNumber, email, password] : [firstName, lastName, phoneNumber, email, password, role];
 
     try {
       const result = await pool.query(query, values);
