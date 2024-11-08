ab29c91b015176e9a80f1ebac394d4e216d7d749
youssef ammar <128386855+ysfd1@users.noreply.github.com>
2024-11-08 14:13:16 +0200

    Add files via upload

---
+++ Frontend/src/Pages/AssignedOrdersPage.js
@@ -0,0 +1,16 @@
+import AssignedOrdersform from "../Component/Courir/AssignedOrdersform";
+import WebsiteTemplate from "../Component/WebsiteTemplate.js/WebsiteTemplate"
+
+function AssignedOrdersPage (){
+    return (
+        <div>
+          <AssignedOrdersform>
+            
+          </AssignedOrdersform>
+           < WebsiteTemplate/>
+
+        </div>
+    )
+
+}
+export default AssignedOrdersPage 
---
+++ Frontend/src/Pages/ListOrdersPage.js
@@ -0,0 +1,16 @@
+import OrderForm from "../Component/Order/OrderForm";
+import WebsiteTemplate from "../Component/WebsiteTemplate.js/WebsiteTemplate"
+
+function ListOrdersPage (){
+    return (
+        <div>
+            <OrderForm>
+
+            </OrderForm>
+           < WebsiteTemplate/>
+
+        </div>
+    )
+
+}
+export default ListOrdersPage 
