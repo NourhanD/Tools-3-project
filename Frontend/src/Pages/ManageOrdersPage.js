a87bbddcd43730f4c0d51054fe33d2bb72a05dd8
youssef ammar <128386855+ysfd1@users.noreply.github.com>
2024-11-09 13:37:37 +0200

    Add files via upload

---
+++ Frontend/src/Pages/ManageOrdersPage.js
@@ -0,0 +1,16 @@
+import ManageOrdersForm from "../Component/Admin/ManageOrdersForm";
+import WebsiteTemplate from "../Component/WebsiteTemplate.js/WebsiteTemplate"
+
+function ManageOrdersPage (){
+    return (
+        <div>
+          <ManageOrdersForm>
+            
+          </ManageOrdersForm>
+           < WebsiteTemplate/>
+
+        </div>
+    )
+
+}
+export default ManageOrdersPage 
