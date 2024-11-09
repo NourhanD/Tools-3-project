902f91b1d7e3fb29dbd8cf34b8f37bd624247adb
Janasobeih <123209222+Janasobeih@users.noreply.github.com>
2024-11-09 09:34:44 +0200

    Update Order.js

--- Backend/Model/Order.js
+++ Backend/Model/Order.js
@@ -36,6 +36,35 @@ class Order {
         console.error('Error fetching order details:', err);
         throw err;
       }
+    }static async cancelOrder(orderId, userId) {
+      const query = `
+        UPDATE "Orders"
+        SET "status" = 'Canceled'
+        WHERE "order_id" = $1 AND "user_id" = $2 AND "status" = 'Pending'
+        RETURNING *;
+
+      `;
+      try {
+        const result = await pool.query(query, [orderId, userId]);
+        if (result.rows.length === 0) {
+          throw new Error('Order cannot be canceled (either not found, unauthorized, or already processed)');
+        }
+        return result.rows[0];
+      } catch (err) {
+        console.error('Error canceling order:', err);
+        throw err;
+      }
+    }
+    static async isCourier(userId) {
+      try {
+        const query = 'SELECT * FROM "Courier" WHERE "id" = $1';
+        const result = await pool.query(query, [userId]);
+  
+        return result.rows.length > 0;
+      } catch (err) {
+        console.error('Error checking if user is a courier:', err);
+        throw err;
+      }
     }
 }
 
