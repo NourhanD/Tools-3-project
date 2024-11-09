d3977ad73321f21ab2f9fa01321ababc77027392
Janasobeih <123209222+Janasobeih@users.noreply.github.com>
2024-11-09 09:35:43 +0200

    Update authentication.js

--- Backend/Middleware/authentication.js
+++ Backend/Middleware/authentication.js
@@ -19,5 +19,28 @@ const authenticate = (req, res, next) => {
         res.status(403).json({ error: 'Invalid token' });
     }
 };
-
-module.exports = authenticate;
+const authenticateAndCheckCourier = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; 
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+      const userId = decoded.id;
+  
+      // Check if user exists in the Couriers table
+      const query = 'SELECT * FROM "Couriers" WHERE "user_id" = $1';
+      const result = await pool.query(query, [userId]);
+  
+      if (result.rows.length === 0) {
+        return res.status(403).json({ error: 'Only couriers are authorized to perform this action' });
+      }
+  
+      req.user = { id: userId }; 
+      next();
+    } catch (err) {
+      res.status(401).json({ error: 'Invalid token' });
+    }
+  };
+module.exports = authenticate,authenticateAndCheckCourier;
