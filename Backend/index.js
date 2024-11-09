b1555be9c6b1ec4251926c05c43cbf98808237f3
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-09 23:21:13 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const userController = require('./Controller/userController');
+const orderController = require('./Controller/orderController');
 const authenticate = require('./Middleware/authentication');
 const authenticateAndCheckCourier = require('./Middleware/authentication');
 const express = require('express');
@@ -12,13 +13,35 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.post('/register', userController.register);
-app.post('/login',userController.login);
-app.post('/order', authenticate, userController.createOrder);
-app.get('/myorders', authenticate, userController.getUserOrders);
-app.get('/myorder/:orderId', authenticate, userController.getOrderById);
-app.put('/updateStatus/:orderId', authenticateAndCheckCourier, userController.updateOrderStatus);
-app.get('/assignedOrders', authenticate, userController.getOrdersWithAssignments);
+
+app.post('/register', userController.register); //customer or courier
+
+app.post('/login',userController.login); //customer or courier or admin
+
+app.post('/order', authenticate, orderController.createOrder) //customer
+
+app.get('/myorders', authenticate, userController.getUserOrders); //customer
+
+app.get('/myorder/:orderId', authenticate, orderController.getOrderById); //customer
+
+app.delete('/order/:orderId/cancel', authenticate, orderController.cancelOrder); //customer
+
+app.get('/courier/orders', authenticateAndCheckCourier, orderController.getAssignedOrders); //courier
+
+app.put('/updateStatus/:orderId', authenticateAndCheckCourier, orderController.updateOrderStatus); //courier
+
+app.get('/assignedOrders', authenticate,userController.isAdmin, orderController.getOrdersWithAssignments); //admin
+
+app.get('/orders', authenticate, userController.isAdmin, orderController.getAllOrders); //admin
+
+app.put('/orderU/:orderId',authenticate, userController.isAdmin, orderController.updateOrderStatusByAdmin); //admin
+
+app.delete('/orderD/:orderId',authenticate, userController.isAdmin, orderController.deleteOrder); //admin
+
+app.get('/neworders', orderController.getPendingOrders); //courier
+
+app.put('/order/:order_id/accept', authenticateAndCheckCourier, orderController.acceptOrder); //courier
+
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
