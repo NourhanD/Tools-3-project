075c22c97da3d17a7913333788d58daa639da808
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-28 22:05:04 +0200

    Update WebsiteTemplate.js

--- Frontend/src/Component/WebsiteTemplate/WebsiteTemplate.js
+++ Frontend/src/Component/WebsiteTemplate/WebsiteTemplate.js
@@ -1,25 +1,35 @@
-import truck from "../Icons/truck.png" 
-import delivery from '../Icons/smiling-deliveryman-with-box-in-hands-png.png'
+import truck from "../Icons/truck.png";
+import delivery from '../Icons/smiling-deliveryman-with-box-in-hands-png.png';
 
-function WebsiteTemplate(){
-    return(
-    <div>
-        <img 
-        src={truck} 
-        className="position-absolute bottom-0 end-0" 
-        style={{ width: "280px", height: "280px" , marginRight:"40px",marginTop:"90px"}} 
-        alt="Truck Icon"
-      />
+function WebsiteTemplate() {
+    return (
+        <div>
+            <img
+                src={truck}
+                className="position-fixed"
+                style={{
+                    width: "280px",
+                    height: "280px",
+                    bottom: "0",
+                    right: "40px",
+                    marginTop: "90px",
+                }}
+                alt="Truck Icon"
+            />
 
-        <img 
-            className="position-absolute bottom-0 start-0" 
-            src={delivery} 
-            style={{ width: "200px", height: "200px" , marginBottom:"30px" , marginLeft:"40px"}} 
-            alt="Deliver man Icon" 
-        />
-
+            <img
+                src={delivery}
+                className="position-fixed"
+                style={{
+                    width: "200px",
+                    height: "200px",
+                    bottom: "30px",
+                    left: "40px",
+                }}
+                alt="Delivery Man Icon"
+            />
         </div>
-    )
+    );
 }
 
-export default WebsiteTemplate
+export default WebsiteTemplate;
