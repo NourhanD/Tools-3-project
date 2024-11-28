d5b82359a019d65dccf49cac31e8ecb4dad870f4
NourhanD <148591826+NourhanD@users.noreply.github.com>
2024-11-28 21:53:54 +0200

    Update UserHomePage.js

--- Frontend/src/Pages/UserHomePage.js
+++ Frontend/src/Pages/UserHomePage.js
@@ -1,15 +1,16 @@
-import WebsiteTemplate from '../Component/WebsiteTemplate/WebsiteTemplate';
+import WebsiteTemplate from "../Component/WebsiteTemplate/WebsiteTemplate"
 import createOrderIcon from '../Component/Icons/orders-icon-5.jpg';
 import previousOrders from '../Component/Icons/Screenshot-2022-11-24-132227.png';
 import { useNavigate } from 'react-router-dom';
 
 function UserHomePage() {
     const navigate = useNavigate()
+    
     return (
         <div>
             <WebsiteTemplate />
             <div style={{ position: "absolute", top: "60px", left: "30px" }}>
-                <h3 style={{ fontSize: "40px" ,fontWeight:"bold" }}>Hello username</h3>
+                <h3 style={{ fontSize: "40px" ,fontWeight:"bold" }}>Hello</h3>
             </div>
             
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", gap: "120px" }}>
